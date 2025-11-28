9b897798d7b198edc940f372e8d10c734f6e672c
SorayaGomesS <sorayagomes62@gmail.com>
2025-11-28 19:43:32 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,80 +1,75 @@
-import os
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from datetime import datetime
+import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SECRET_KEY'] = 'troque-por-uma-chave-secreta'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-
-class Role(db.Model):
-    __tablename__ = 'roles'
+# Model
+class Curso(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+    nome = db.Column(db.String(120), nullable=False)
+    descricao = db.Column(db.String(500))
 
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return f'<Curso {self.nome}>'
 
+# Form
+class CursoForm(FlaskForm):
+    nome = StringField('Qual é o nome do curso?', validators=[DataRequired()])
+    descricao = TextAreaField('Descrição (250 caracteres)', validators=[DataRequired()])
+    submit = SubmitField('Salvar')
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
-@app.route('/', methods=['GET', 'POST'])
+# Rotas obrigatórias
+@app.route('/')
 def index():
-    form = NameForm()
+    # Nome e prontuário pedidos no enunciado
+    aluno = "Soraya Gomes da Silva"
+    prontuario = "PT3032515"
+    # data/hora local (ISO ou formatada) - usamos datetime local
+    now_local = datetime.now()
+    return render_template('index.html',
+                           aluno=aluno,
+                           prontuario=prontuario,
+                           now_local=now_local)
+
+@app.route('/cursos', methods=['GET', 'POST'])
+def cursos():
+    form = CursoForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+        novo = Curso(nome=form.nome.data, descricao=form.descricao.data)
+        db.session.add(novo)
+        db.session.commit()
+        return redirect(url_for('cursos'))
+    cursos = Curso.query.all()
+    return render_template('cursos.html', form=form, cursos=cursos)
+
+# Rotas mínimas para módulos não desenvolvidos (mostram "Não disponível")
+@app.route('/professores')
+@app.route('/disciplinas')
+@app.route('/alunos')
+@app.route('/ocorrencias')
+def nao_disponivel():
+    return render_template('nao_disponivel.html'), 200
+
+# Cria o DB se não existir
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
